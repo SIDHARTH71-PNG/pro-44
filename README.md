a2cc51c6b6bc07b2a9fda939328fb980f6001183
SIDHARTH71-PNG <69514690+SIDHARTH71-PNG@users.noreply.github.com>
2021-01-09 14:15:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pro-44
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+var forestImg;
+
+
+
+
+function preload()
+{
+forestImg=loadImage("5ff841054a6c6.jpg");
+}
+
+function setup()
+{
+createCanvas(1000,1000);
+
+}
+
+
+function draw()
+{
+
+ background(forestImg);
+}
